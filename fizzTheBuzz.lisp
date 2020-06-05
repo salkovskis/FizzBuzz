f2acd61238df9d7c479823d3aaa8fc3a1878718b
lemurs994 <lemurs994@gmail.com>
2020-06-05 17:35:15 +0300

    add fizzTheBuzz.lisp

---
+++ fizzTheBuzz.lisp
@@ -0,0 +1,19 @@
+(defun getFizzBuzzString (i) 
+    
+    ( if(= (mod i 15 ) 0)
+        (return-from getFizzBuzzString "FizzBuzz")
+        ( if(= (mod i 3) 0)
+            (return-from getFizzBuzzString "Fizz")
+            ( if(= (mod i 5) 0)
+                (return-from getFizzBuzzString "Buzz")
+                (return-from getFizzBuzzString 
+                    i
+                )
+            )
+        )
+    )
+    
+)
+
+
+(loop for i from 1 to 30 do ( format t "~d~%" (getFizzBuzzString i)))
